72dd27626ccf5f4ce649f1f7d95212c706aa6706
Maximilian Bösing <2189546+boesing@users.noreply.github.com>
2022-08-10 18:30:25 +0200

    qa: update `composer.lock`
    This also adds a conflict with `laminas-servicemanager` <3.11 to ensure we can remove `container-interop`
    
    Signed-off-by: Maximilian Bösing <2189546+boesing@users.noreply.github.com>

--- test/integration/Psr/SimpleCache/BlackHoleIntegrationTest.php
+++ test/integration/Psr/SimpleCache/BlackHoleIntegrationTest.php
@@ -13,32 +13,35 @@ final class BlackHoleIntegrationTest extends AbstractSimpleCacheIntegrationTest
 {
     private const TEST_NOT_SUPPORTED_REASON = 'BlackHole never caches.';
 
-    /** @var array<string,string> */
-    protected $skippedTests = [
-        'testSet'                            => self::TEST_NOT_SUPPORTED_REASON,
-        'testSetMultipleValidData'           => self::TEST_NOT_SUPPORTED_REASON,
-        'testSetValidData'                   => self::TEST_NOT_SUPPORTED_REASON,
-        'testSetMultipleValidKeys'           => self::TEST_NOT_SUPPORTED_REASON,
-        'testSetValidKeys'                   => self::TEST_NOT_SUPPORTED_REASON,
-        'testDataTypeObject'                 => self::TEST_NOT_SUPPORTED_REASON,
-        'testDataTypeArray'                  => self::TEST_NOT_SUPPORTED_REASON,
-        'testDataTypeBoolean'                => self::TEST_NOT_SUPPORTED_REASON,
-        'testDataTypeFloat'                  => self::TEST_NOT_SUPPORTED_REASON,
-        'testDataTypeInteger'                => self::TEST_NOT_SUPPORTED_REASON,
-        'testDataTypeString'                 => self::TEST_NOT_SUPPORTED_REASON,
-        'testHas'                            => self::TEST_NOT_SUPPORTED_REASON,
-        'testGetMultipleWithGenerator'       => self::TEST_NOT_SUPPORTED_REASON,
-        'testSetMultipleWithGenerator'       => self::TEST_NOT_SUPPORTED_REASON,
-        'testGetMultiple'                    => self::TEST_NOT_SUPPORTED_REASON,
-        'testSetMultipleTtl'                 => self::TEST_NOT_SUPPORTED_REASON,
-        'testSetMultiple'                    => self::TEST_NOT_SUPPORTED_REASON,
-        'testGet'                            => self::TEST_NOT_SUPPORTED_REASON,
-        'testSetTtl'                         => self::TEST_NOT_SUPPORTED_REASON,
-        'testObjectDoesNotChangeInCache'     => self::TEST_NOT_SUPPORTED_REASON,
-        'testBasicUsageWithLongKey'          => self::TEST_NOT_SUPPORTED_REASON,
-        'testBinaryData'                     => self::TEST_NOT_SUPPORTED_REASON,
-        'testSetMultipleWithIntegerArrayKey' => self::TEST_NOT_SUPPORTED_REASON,
-    ];
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->skippedTests = [
+            'testSet'                            => self::TEST_NOT_SUPPORTED_REASON,
+            'testSetMultipleValidData'           => self::TEST_NOT_SUPPORTED_REASON,
+            'testSetValidData'                   => self::TEST_NOT_SUPPORTED_REASON,
+            'testSetMultipleValidKeys'           => self::TEST_NOT_SUPPORTED_REASON,
+            'testSetValidKeys'                   => self::TEST_NOT_SUPPORTED_REASON,
+            'testDataTypeObject'                 => self::TEST_NOT_SUPPORTED_REASON,
+            'testDataTypeArray'                  => self::TEST_NOT_SUPPORTED_REASON,
+            'testDataTypeBoolean'                => self::TEST_NOT_SUPPORTED_REASON,
+            'testDataTypeFloat'                  => self::TEST_NOT_SUPPORTED_REASON,
+            'testDataTypeInteger'                => self::TEST_NOT_SUPPORTED_REASON,
+            'testDataTypeString'                 => self::TEST_NOT_SUPPORTED_REASON,
+            'testHas'                            => self::TEST_NOT_SUPPORTED_REASON,
+            'testGetMultipleWithGenerator'       => self::TEST_NOT_SUPPORTED_REASON,
+            'testSetMultipleWithGenerator'       => self::TEST_NOT_SUPPORTED_REASON,
+            'testGetMultiple'                    => self::TEST_NOT_SUPPORTED_REASON,
+            'testSetMultipleTtl'                 => self::TEST_NOT_SUPPORTED_REASON,
+            'testSetMultiple'                    => self::TEST_NOT_SUPPORTED_REASON,
+            'testGet'                            => self::TEST_NOT_SUPPORTED_REASON,
+            'testSetTtl'                         => self::TEST_NOT_SUPPORTED_REASON,
+            'testObjectDoesNotChangeInCache'     => self::TEST_NOT_SUPPORTED_REASON,
+            'testBasicUsageWithLongKey'          => self::TEST_NOT_SUPPORTED_REASON,
+            'testBinaryData'                     => self::TEST_NOT_SUPPORTED_REASON,
+            'testSetMultipleWithIntegerArrayKey' => self::TEST_NOT_SUPPORTED_REASON,
+        ];
+    }
 
     protected function createStorage(): StorageInterface
     {
